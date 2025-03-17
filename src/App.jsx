fac20794b7345a9ee2659e7ee69b9806bd6ba89a
user <olgalachowska.work@gmail.com>
2025-03-17 20:22:19 +0100

    Pages + React Router + CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
