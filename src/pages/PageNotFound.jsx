3ea956fbf74be5847669fe3b63f6d37faa3a45a7
user <olgalachowska.work@gmail.com>
2025-03-19 14:13:20 +0100

    pages

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <p>Not Found</p>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
