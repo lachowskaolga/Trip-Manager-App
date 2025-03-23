652e1fcc495133d7e92dbfcada2dbabb0773471f
user <olgalachowska.work@gmail.com>
2025-03-23 08:33:24 +0100

    Nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,11 @@ export default function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Homepage />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
